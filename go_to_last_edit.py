25d23465ef0a995e762714afd16fd1b0d43ead3c
Zoltan Szugyi <zoli.szugyi@gmail.com>
2011-11-25 15:22:27 -0800

    adds command to jump to the last edit location

---
+++ go_to_last_edit.py
@@ -0,0 +1,33 @@
+import sublime, sublime_plugin
+
+last_point = None
+last_view_id = None
+
+class EditListener(sublime_plugin.EventListener):
+	def on_modified(self, view): 
+		global last_point
+		global last_view_id
+
+		last_view_id = view.id() 
+		last_point = view.sel()[0]
+
+class GoToLastEditCommand(sublime_plugin.WindowCommand):
+
+	def run(self):
+		global last_point
+		global last_view_id
+
+		if last_point is None or last_view_id is None:
+			print "no previous edit location"
+		else:
+			window = sublime.active_window()
+			views = window.views()
+			last_view = filter(lambda view: view.id() == last_view_id, views)
+			if last_view:
+				view = last_view[0]
+				window.focus_view(view)
+				view.sel().clear()
+				view.sel().add(last_point)
+				view.show(last_point) # why u no work?
+			else:
+				print "last view not found"
